4343fd0bd8743418aaa65e48390642f84cb2518f
taylorbot <dev@giantswarm.io>
2023-05-02 08:57:35 +0000

    Release v1.8.2

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "aws-pod-identity-webhook"
 	source      = "https://github.com/giantswarm/aws-pod-identity-webhook"
-	version     = "1.8.1"
+	version     = "1.8.2"
 )
 
 func Description() string {
