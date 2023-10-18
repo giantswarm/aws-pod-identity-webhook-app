ea2558be60b4a658de4ed8be53b7ec019ae4a069
taylorbot <dev@giantswarm.io>
2023-10-18 10:23:06 +0000

    Bump version to 1.13.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "aws-pod-identity-webhook"
 	source      = "https://github.com/giantswarm/aws-pod-identity-webhook"
-	version     = "1.13.1"
+	version     = "1.13.2-dev"
 )
 
 func Description() string {
